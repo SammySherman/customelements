47669c2daae3ff697051fc0127b9cf385deee619
Sammy Sherman <ssherman12345@gmail.com>
2020-11-04 23:04:52 -0600

    Add angular sample

---
+++ Samples/angular/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'angular';
+  disabled = false;
+  buttons = [
+    [ { text: 'Basic' }, { text: 'Primary', color: 'primary', class: 'custom-primary' }, { text: 'Accent', color: 'accent' }, { text: 'Disabled', disabled: true } ],
+    [ { text: 'Basic', appearance: 'outline' }, { text: 'Primary', color: 'primary', appearance: 'outline' }, { text: 'Accent', color: 'accent', appearance: 'outline' }, { text: 'Disabled', disabled: true, appearance: 'outline' } ],
+    [ { text: 'Basic', appearance: 'flat' }, { text: 'Primary', color: 'primary', appearance: 'flat' }, { text: 'Accent', color: 'accent', appearance: 'flat' }, { text: 'Disabled', disabled: true, appearance: 'flat' } ],
+  ];
+  buttonClick(button: any): void {
+    console.log('Button clicked:', button);
+  }
+}
---
+++ Samples/angular/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
